ddd06722cb4ce004ab4b43996f2c0ec1be4894fe
GetVybz <prestigecollegeofmusic@gmail.com>
2025-09-04 21:28:20 +0100

    Fix require syntax

--- index (2).js
+++ index (2).js
@@ -1,2 +1,33 @@
-import express from 'express';
-const app=express();app.use(express.json());app.get('/',(r,s)=>s.send('GetVybz API running'));app.listen(4000,()=>console.log('listening'));
+const express = require('express');
+const connectDB = require('./src/config/db');
+const path = require('path');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/auth', require('./src/routes/authRoutes'));
+
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
