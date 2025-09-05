8e5c2b28687c3899d3244e099c0f2bd32ab2b5fa
GetVybz <prestigecollegeofmusic@gmail.com>
2025-09-05 11:05:43 +0100

    Corrected all file paths

--- index (2).js
+++ index (2).js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => res.send('API Running'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
