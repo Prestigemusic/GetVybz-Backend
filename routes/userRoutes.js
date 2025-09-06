4a6c34eb595662fb071ffb0487531818531b268e
GetVybz <prestigecollegeofmusic@gmail.com>
2025-09-06 12:19:08 +0100

    Updated userRoutes file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/userController');
 
-// The route for user registration
+// The route for user registration - updated
 router.post('/signup', registerUser);
 
 module.exports = router;
