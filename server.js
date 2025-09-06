381dfcdbe95931339ac5d561016c55b3ec267adc
GetVybz <prestigecollegeofmusic@gmail.com>
2025-09-06 16:52:53 +0100

    Force deploy with correct auth routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./models');
-const authRoutes = require('./routes/auth'); // FIXED
+const authRoutes = require('./routes/auth'); // FIXED - updated
 const profileRoutes = require('./routes/profile');
 
 const app = express();
